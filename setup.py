db69f094a2571e6cb1441db4d5709651a754bff6
srounet <srounet@gmail.com>
2011-10-27 16:47:47 +0200

    add setup installer

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version='1.0'
+
+setup(
+    name = 'gt5bot',
+    version = version,
+    description = 'Gran turismo 5 bspec bot',
+    author = 'Fabien Reboia',
+    author_email = 'srounet@gmail.com',
+    maintainer = 'Fabien Reboia',
+    license = 'beer-ware',
+    url = 'http://github.com/srounet/gt5bot',
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Other Audience",
+        "License :: Beerware",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 2",
+        "Topic :: Games/Entertainment :: Automation",
+    ]
+)
